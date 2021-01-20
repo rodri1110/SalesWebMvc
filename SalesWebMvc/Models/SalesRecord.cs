94918ac7fb0e43012a44c2f2b0d71b1ced005c7f
Rodri1110 <rodri.oliveira1110@gmail.com>
2021-01-20 14:48:40 -0300

    Novas entidades e segunda Migration

---
+++ SalesWebMvc/Models/SalesRecord.cs
@@ -0,0 +1,29 @@
+﻿using SalesWebMvc.Models.Enums;
+using System;
+
+namespace SalesWebMvc.Models
+{
+    public class SalesRecord
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public double Amount { get; set; }
+        public SaleStatus Status { get; set; }
+
+        //Insere relacionamento 1 pra muitos (1 x M), onde 1 vendedor pode ter muitos salesRecords
+        public Seller Seller { get; set; }
+
+        public SalesRecord()
+        {
+        }
+
+        public SalesRecord(int id, DateTime date, double amount, SaleStatus status, Seller seller)
+        {
+            Id = id;
+            Date = date;
+            Amount = amount;
+            Status = status;
+            Seller = seller;
+        }
+    }
+}
